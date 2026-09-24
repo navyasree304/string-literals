c6a644d7bb3a5dabbb39fb1aa8510700b5a50358
navyasree304 <24kb1a05fk@nbkrist.org>
2026-09-24 10:02:47 +0530

    Update and rename README.md to react.js

---
+++ react.js
@@ -0,0 +1,10 @@
+import React from "react";
+function StringLiterals() { const name = "NavyaSree"; const age = 19;
+return (
+<div>
+<h2>Using String Literals</h2>
+<p>Hello, my name is {name} and I am {age} years old.</p>
+</div>
+);
+}
+export default StringLiterals;
